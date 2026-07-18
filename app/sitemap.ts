6539022c3d91cb79566216b99322b7eaf027fa5b
Khoa1512 <danggkhoaa1512@gmail.com>
2026-07-18 17:24:17 +0700

    fix(seo): include project detail pages in the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 import { site } from '@/content/site';
+import { projects } from '@/content/projects';
 import { getAllPosts } from '@/lib/mdx';
 
 export default function sitemap(): MetadataRoute.Sitemap {
@@ -9,9 +10,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
     lastModified: new Date(post.date),
   }));
 
+  const projectEntries = projects.map((project) => ({
+    url: `${site.url}/projects/${project.id}`,
+    lastModified: new Date(),
+  }));
+
   return [
     { url: site.url, lastModified: new Date() },
     { url: `${site.url}/blog`, lastModified: new Date() },
     ...posts,
+    ...projectEntries,
   ];
 }
